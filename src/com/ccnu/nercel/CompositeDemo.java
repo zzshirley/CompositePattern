8761ae29ecbb7eba51083a2be0a040eb0fe95809
晓彤 <xiaotong@xiaotongdeMacBook-Pro.local>
2018-06-01 08:50:17 +0800

    main

--- src/com/ccnu/nercel/CompositeDemo.java
+++ src/com/ccnu/nercel/CompositeDemo.java
@@ -1,31 +1,49 @@
 package com.ccnu.nercel;
 
 import java.io.File;
+import java.io.FileFilter;
 
 public class CompositeDemo {
 	public static void main(String[] args) {
-		Inode inoder =new Inoder("/Documents");
+		Inode inoder =new Inoder("/Users/xiaotong/Documents/sequencepattern");
+		String keyword="data9.txt";
 		try {
-			createtree(inoder);
-			} catch (Exception e) {
-				e.printStackTrace();
-				}
-		inoder.display();		
-	}
-	public static void createtree(Inode inode) throws Exception{
-		File file = new File(inode.nodename);
-		File[] f=file.listFiles();
-		for(File fi :f) {
-			if(fi.isFile()) {
-				file filer =new file(fi.getAbsolutePath());
-				inode.add(filer);
-			}
-			if(fi.isDirectory()) {
-				Inoder inoder = new Inoder(fi.getAbsolutePath());
-				inode.add(inoder);
-				createtree(inoder);
-			}
+			searchfile(inoder,keyword);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
+		inoder.display();
 	}
-	
-}
+	public static void searchfile(Inode inode,String name) throws Exception{
+		File file = new File(inode.nodename);
+		File[] f=file.listFiles(
+				new FileFilter() {
+		            @Override
+		            public boolean accept(File file) {
+		               if (file.isDirectory()) {
+		                    return true;
+		                }
+		                if (file.getName().toLowerCase().contains(name)) {
+		                    return true;		                    
+		                }
+		                return false;		                
+		                }
+		            }
+				);
+		if(f!=null) {
+			for(File fi :f) {
+				if(fi.isFile()) {
+					file filer =new file(fi.getAbsolutePath());
+					inode.add(filer);	
+				}
+				if(fi.isDirectory()) {
+					Inoder inoder = new Inoder(fi.getAbsolutePath());
+					inode.add(inoder);
+					searchfile(inoder,name);
+					}
+			}
+         }
+	}
+}	
+
